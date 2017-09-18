c3550c03e57e97812f052382b0523368aa0d5c60
hansheng quek <hsquek06@gmail.com>
2017-09-18 18:46:46 +0800

    testing cron

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var cron = require('node-cron')
 
 var users = require('./routes/users')
+
+cron.schedule('* * * * *', function () {
+  console.log('===========================')
+  console.log('running a task every minute')
+  console.log('===========================')
+})
 
 var app = express()
 
